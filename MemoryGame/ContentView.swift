e392593a4d24c49148304ba8d20ea4bd82594303
David Castaneda <Davidcas7@hotmail.com>
2026-03-18 16:05:52 -0400

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by David Castaneda on 3/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by David Castaneda on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
